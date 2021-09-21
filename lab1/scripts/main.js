7062f3a3ef153c6cbab53e6d1638f805fa1faee5
ykvlv <ykvlv@niuitmo.ru>
2021-09-21 15:53:54 +0300

    js check completed

--- lab1/scripts/main.js
+++ lab1/scripts/main.js
@@ -1,5 +1,5 @@
 const in_Y = document.querySelector("input[id=in_Y]");
-const sl_R = document.querySelector("input[id=sl_R]");
+const sl_R = document.querySelector("select[id=sl_R]");
 const ch_X = document.querySelectorAll("input[name=choose_X]");
 
 const error_in_Y = document.querySelector("span[id=error_in_Y]");
@@ -7,36 +7,45 @@ const error_sl_R = document.querySelector("span[id=error_sl_R]");
 const error_ch_X = document.querySelector("span[id=error_ch_X]");
 
 function validateForm() {
-    let formValid = true;
+    let xValid = false;
+    let yValid = false;
+    let rValid = false;
 
+    // x validation
     for (let i = 0; i < ch_X.length; i++) {
-        if (!ch_X[i].checked) {
-            error_ch_X.textContent = "Выберите значение из предложенных."
-            return false;
+        if (ch_X[i].checked) {
+            xValid = true;
+            error_ch_X.textContent = "";
         }
     }
-
-    let value_in_Y = in_Y.value.replace(",", ".");
-    if (value_in_Y === undefined) {
-        error_in_Y.textContent = "Поле Y пустое."
-        return false;
-    }
-    if (isNaN(parseFloat(value_in_Y)) || !isFinite(value_in_Y)) {
-        error_in_Y.textContent = "X должен быть числом от -4.5 до 4.5!"
-        return false;
-    }
-    if (value_in_Y < 15) {
-        addToErrorMessage(`Длина числа должна быть не более ${maxLength} символов`);
-        return false;
-    }
-    if (!((x > -3) && (x < 5))) {
-        addToErrorMessage("Нарушена область допустимых значений X (-3; 5)");
-        return false;
+    if (!xValid) {
+        error_ch_X.textContent = "Выберите значение из предложенных";
     }
 
-    x = document.querySelector("input[id=xCoordinate]").value.replace(",", ".");
+    // y validation
+    let y = in_Y.value.replace(",", ".");
+    if (y === "" || y === undefined) {
+        error_in_Y.textContent = "Поле Y пустое";
 
-}
+    } else if (isNaN(parseFloat(y)) || !isFinite(y)) {
+        error_in_Y.textContent = "Y — число";
 
-function showError() {
+    } else if (y.length > 15) {
+        error_in_Y.textContent = "Длина Y не более 15 символов";
+
+    } else if (y <= -4.5 || y >= 4.5) {
+        error_in_Y.textContent = "Y в пределах (-4.5; 4.5)";
+    } else {
+        yValid = true;
+        error_in_Y.textContent = "";
+    }
+
+    // r validation
+    if (sl_R.value === "value0") {
+        error_sl_R.textContent = "Выберите значение из предложенных"
+    } else {
+        error_sl_R.textContent = "";
+        rValid = true;
+    }
+    return xValid && yValid && rValid;
 }
