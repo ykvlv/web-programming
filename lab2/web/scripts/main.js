41cd71c57e3837fc334021d08950a405e3054806
Grigory Yakovlev <ykvlv@niuitmo.ru>
2021-10-20 21:47:33 +0300

    JS improved

--- lab2/web/scripts/main.js
+++ lab2/web/scripts/main.js
@@ -1,51 +1,72 @@
-const in_Y = document.querySelector("input[id=in_Y]");
-const sl_R = document.querySelector("select[id=sl_R]");
-const ch_X = document.querySelectorAll("input[name=x]");
-const Y_MAX_LENGTH = 15, Y_MIN = -3, Y_MAX = 5;
-let x, y, r;
+const error_Y = document.querySelector("span[id=error_Y]");
+const error_R = document.querySelector("span[id=error_R]");
+const error_X = document.querySelector("span[id=error_X]");
 
-const error_in_Y = document.querySelector("span[id=error_in_Y]");
-const error_sl_R = document.querySelector("span[id=error_sl_R]");
-const error_ch_X = document.querySelector("span[id=error_ch_X]");
+const in_Y = document.querySelector("input[id=valueY]");
+const Y_MAX_LENGTH = 15, Y_MIN = -5, Y_MAX = 3;
+document.getElementById("valueY").setAttribute("placeholder", `от ${Y_MIN} до ${Y_MAX}`);
+
+let x, y, r;
+let previousX, previousR;
+
+$("input[name=x]").click(function() {
+    if (previousX !== null && previousX !== undefined) {
+        previousX.style.borderColor = "black";
+    }
+    error_X.textContent = "";
+    this.style.borderColor = "rgb(212, 87, 75)";
+    x = this.value;
+    previousX = this;
+});
+
+$("input[name=r]").click(function() {
+    if (previousR !== null && previousR !== undefined) {
+        previousR.style.borderColor = "black";
+    }
+    error_R.textContent = "";
+    this.style.borderColor = "rgb(212, 87, 75)";
+    r = this.value;
+    previousR = this;
+});
+
+$("input[id=valueY]").keyup(function () {
+    validateY();
+})
+
 
 function validateX() {
-    for (let i = 0; i < ch_X.length; i++) {
-        if (ch_X[i].checked) {
-            x = ch_X[i].value
-            error_ch_X.textContent = "";
-            return true;
-        }
+    if (x === null || x === undefined) {
+        error_X.textContent = "Выберите значение из предложенных";
+        return false;
     }
-    error_ch_X.textContent = "Выберите значение из предложенных";
-    return false;
+    error_X.textContent = "";
+    return true;
 }
 
 function validateY() {
     y = in_Y.value.replace(",", ".");
     if (y === undefined || y === "") {
-        error_in_Y.textContent = "Поле Y пустое";
+        error_Y.textContent = "Поле Y пустое";
     } else if (isNaN(parseFloat(y)) || !isFinite(y)) {
-        error_in_Y.textContent = "Y — число";
+        error_Y.textContent = "Y — число";
     } else if (y.length > Y_MAX_LENGTH) {
-        error_in_Y.textContent = "Длина Y не более 15 символов";
-    } else if (y <= Y_MIN || y >= Y_MAX) {
-        error_in_Y.textContent = "Y в пределах (-3 ... 5)";
+        error_Y.textContent = `Длина Y не более ${Y_MAX_LENGTH} символов`;
+    } else if (y < Y_MIN || y > Y_MAX) {
+        error_Y.textContent = `Y в пределах (${Y_MIN} ... ${Y_MAX})`;
     } else {
-        error_in_Y.textContent = "";
+        error_Y.textContent = "";
         return true;
     }
     return false;
 }
 
 function validateR() {
-    r = sl_R.value;
-    if (r === undefined || r === "not_chosen") {
-        error_sl_R.textContent = "Выберите значение из предложенных"
-    } else {
-        error_sl_R.textContent = "";
-        return true;
+    if (r === null || r === undefined) {
+        error_R.textContent = "Выберите значение из предложенных";
+        return false;
     }
-    return false;
+    error_R.textContent = "";
+    return true;
 }
 
 function validateForm() {
