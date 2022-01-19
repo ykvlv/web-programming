15c031c4177746a032dd2c031b3877118e39266e
Grigory Yakovlev <ykvlv@niuitmo.ru>
2022-01-19 10:32:54 +0300

    templates added

---
+++ lab4/src/main/java/ykvlv/lab4/controller/MvcController.java
@@ -0,0 +1,30 @@
+package ykvlv.lab4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MvcController {
+
+    @GetMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping(value = "/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @GetMapping(value = "/application")
+    public String application() {
+        return "application";
+    }
+
+}
